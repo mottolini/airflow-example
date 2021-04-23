8f4e4fe4d48ad6ca6ed0acff3a79fff7f2897bb3
Marco Ottolini <marco@ottolini.com>
2021-04-23 19:38:41 +0200

    Update sample-compactor.py

--- sample-compactor.py
+++ sample-compactor.py
@@ -1,4 +1,3 @@
-from kubernertes.client import models as k8s
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
