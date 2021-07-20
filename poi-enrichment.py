61e7b97fb85c72e40b1103d2bb9de7d85aa352fc
skalogerakis <skalogerakis97@gmail.com>
2021-07-20 14:39:06 +0300

    Update poi-enrichment.py

--- poi-enrichment.py
+++ poi-enrichment.py
@@ -36,10 +36,6 @@ passing = KubernetesPodOperator(namespace='spark',
                                     "WRITE_PATH_COMPACTOR": "s3a://proximai-data/datalake/inferreddata/enrichedPoi/"
                                    },
                           arguments=[
-                              "--conf", "spark.driver.AWS_ACCESS_KEY_ID=admin",
-                              "--conf", "spark.driver.AWS_SECRET_ACCESS_KEY=WS46advKR",
-                              "--conf", "spark.driver.AWS_S3_ENDPOINT=http://minio.vvp.svc.cluster.local:9000",
-                              "--conf", "spark.hadoop.fs.s3a.path.style.access=true",
                               "--verbose",
                               "--deploy-mode", "client",
                               "--class", "ai.proxim.PoiEnrichment",
